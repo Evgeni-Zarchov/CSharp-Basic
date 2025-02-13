345b1751edf909a82a5017efb9973a7050a512c5
ezarchov@gmail.com <ezarchov@gmail.com>
2025-02-13 14:04:34 +0200

    PB-MoreExe

---
+++ PB-MoreExerciseFirstStepInCoding/03-CelsiusToFarenheit/Program.cs
@@ -0,0 +1,7 @@
+﻿double celsium = double.Parse(Console.ReadLine());
+
+double fahrenheit = (celsium * 9 / 5) + 32;
+
+double finalResult = fahrenheit - 32 * (5 / 9);
+
+Console.WriteLine($"{finalResult:f2}");
---
+++ PB-MoreExerciseFirstStepInCoding/04-VegetableMarket/Program.cs
@@ -0,0 +1,15 @@
+﻿double euro = 1.94;
+
+double veggyPrice = double.Parse(Console.ReadLine());
+double fruitsPrice = double.Parse(Console.ReadLine());
+int veggyKilo = int.Parse(Console.ReadLine());
+int fruitsKilo = int.Parse(Console.ReadLine());
+
+double veggyCost = veggyPrice * veggyKilo;
+double fruitsCost = fruitsPrice * fruitsKilo;
+
+double finalPrice = veggyCost + fruitsCost;
+
+double priceIntoEuro = finalPrice / euro;
+
+Console.WriteLine($"{priceIntoEuro:f2}");
