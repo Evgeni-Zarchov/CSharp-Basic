9e199949b752c50899d926d1ae2d2b9876f66223
ezarchov@gmail.com <ezarchov@gmail.com>
2025-02-15 20:02:32 +0200

    WhileLoop-Lab

---
+++ PB-MoreExerciseFirstStepInCoding/05-TrainingLab/Program.cs
@@ -0,0 +1,14 @@
+﻿double height = double.Parse(Console.ReadLine());
+double width = double.Parse(Console.ReadLine());
+
+height *= 100;
+width *= 100;
+
+double h = Math.Floor(height / 120);
+double w = Math.Floor((width - 100) / 70);
+
+
+double finalResult = (w * h) - 3;
+
+Console.WriteLine(finalResult);
+
